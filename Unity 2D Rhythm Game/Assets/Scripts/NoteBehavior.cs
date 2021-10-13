97309dc9f03a757337d4da6f6f438fb3d2def6d8
HwangSahun <92228700+HwangSahun@users.noreply.github.com>
2021-10-14 02:53:44 +0900

    Update NoteBehavior.cs

--- Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteBehavior.cs
@@ -1,4 +1,4 @@
-﻿//노트 판정 및 파괴
+﻿// 노트 판정 및 파괴
 
 using System.Collections;
 using System.Collections.Generic;
@@ -27,14 +27,17 @@ public class NoteBehavior : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
-        
+        // 사용자가 노트 키를 입력한 경우
         if(Input.GetKey(keyCode))
         {
+            // 해당 노트에 대한 판정 진행
             Debug.Log(judge);
+            // 노트가 판정 선에 닿기 시작한 이후 해당 노트 제거
             if (judge != GameManager.judges.NONE) gameObject.SetActive(false);
         }
     }
 
+    // 각 노트의 현재 위치에 대해 판정을 수행
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Bad Line")
